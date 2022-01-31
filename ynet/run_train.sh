67702b3596e3b1ad256bdcebdfe6a9c2c735a9d3
scitas <yuejiang.liu@epfl.ch>
2022-01-31 01:34:56 +0100

    minor script update

--- ynet/run_train.sh
+++ ynet/run_train.sh
@@ -1,12 +1,11 @@
 # mkdir pretrained_models
 
-CKPT='pretrained_models/Seed_1_Train_(0.25_0.75)_(1.25_1.75)_(3.25_3.75)_Val_(0.25_0.75)_(1.25_1.75)_(3.25_3.75)_args.val_ratio_0.3_dataset_ped_gap_weights.pt'
+DATASET=dataset_ped_biker
 
-python train_SDD_trajnet.py --dataset='dataset_ped_biker'
+CKPT=ckpt/Seed_1_Train__0.25_0.75__1.25_1.75__3.25_3.75__Val__0.25_0.75__1.25_1.75__3.25_3.75__Val_Ratio_0.3_dataset_ped_gap_weights.pt
+
+python train_SDD_trajnet.py --dataset=${DATASET} --ckpt=${CKPT}
 
 # python train_SDD_trajnet.py --dataset='dataset_biker'
 
 # python train_SDD_trajnet.py --dataset='dataset_ped'
-
-
-# python train_SDD_trajnet.py --batch_size=60 --ckpt='pretrained_models/Seed_1_Train_(0.25_0.75)_(1.25_1.75)_(3.25_3.75)_Val_(0.25_0.75)_(1.25_1.75)_(3.25_3.75)_args.val_ratio_0.3_dataset_ped_gap_weights.pt'
