f2103eb0ea1901eaeb8a072e91852811fae619ae
Sherwin Bahmani <66998830+sherwinbahmani@users.noreply.github.com>
2022-02-26 17:13:24 +0100

    Update create_custom_dataset.sh

--- create_custom_dataset.sh
+++ create_custom_dataset.sh
@@ -2,6 +2,6 @@ data_raw=/path/to/sdd_ynet/dataset_raw # See README.md on how to download the ra
 step=12
 window_size=20
 stride=20
-data_filter=/path/to/sdd_ynet/dataset_filter_custom # Path to new dataset in sdd_ynet directory
+data_filter=sdd_ynet/dataset_filter_custom # Path to new dataset in sdd_ynet directory
 labels=(Pedestrian Biker) # Choose a subset from: Biker, Bus, Car, Cart, Pedestrian, Skater
 python utils/dataset.py --data_raw $data_raw --step $step --window_size $window_size --stride $stride --data_filter $data_filter --labels $labels
